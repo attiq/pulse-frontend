be800dafcce6270ab7e31d191af9e83747f47a3d
Attiq Rao <arao@experfy.com>
2024-02-01 18:22:28 +0500

    Fixed history

--- src/components/patients/StageHistoryRow.tsx
+++ src/components/patients/StageHistoryRow.tsx
@@ -1,11 +1,23 @@
 import { Grid, IconButton, Stack, Typography, capitalize } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import { useState } from 'react';
 import { convertTimeZone } from '../../helpers/Helpers';
 import TrashIcon from '../../assets/images/icon_trash.svg';
 import TrashIconActive from '../../assets/images/icon_trash_active.svg';
 
+const useStyles = makeStyles({
+  historyRow: {
+    height: '2rem',
+  },
+  innerText: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
+
 export const StageHistoryRow = (props: any) => {
   const { stage, opportunity, selectedStages, setSelectedStages } = props;
+  const classes = useStyles();
   const { timeZone } = Intl.DateTimeFormat().resolvedOptions();
   const [isHoverTrash, setIsHoverTrash] = useState(false);
 
@@ -15,12 +27,12 @@ export const StageHistoryRow = (props: any) => {
 
   return (
     <>
-      <Stack direction={'row'}>
-        <Grid item xs={4}>
+      <Stack direction={'row'} className={classes.historyRow}>
+        <Grid item xs={4} className={classes.innerText}>
           <Typography variant='body1'>{`${capitalize(stage)}`}</Typography>
         </Grid>
 
-        <Grid item xs={6}>
+        <Grid item xs={6} className={classes.innerText}>
           <Typography variant='body1'>{`${convertTimeZone(`${opportunity.stage_history[stage]}`, timeZone)}`}</Typography>
         </Grid>
         <Grid item xs={2}>
